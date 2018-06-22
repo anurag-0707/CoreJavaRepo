6cfedeb515597fa7cc94a101c6530fad1716a10d
Anurag <anu.s1104@gmail.com>
2018-06-22 23:05:51 +0530

    InfiniteStream Uncommented Statement

--- CoreJava/src/java8NewFeatures/InfiniteStream.java
+++ CoreJava/src/java8NewFeatures/InfiniteStream.java
@@ -14,41 +14,38 @@ public class InfiniteStream {
 
 	public static void main(String[] args) {
 
-		/*
-		 * Supplier<UUID> uuid = UUID::randomUUID; Stream<UUID> generate =
-		 * Stream.generate(uuid); List<UUID> list =
-		 * generate.limit(10).skip(15).collect(Collectors.toList()); list.forEach(l ->
-		 * System.out.println(l));
-		 * 
-		 * System.out.println("------------------------");
-		 * 
-		 * Stream<Integer> iterate=Stream.iterate(0, i->i+1);
-		 * iterate.skip(30).limit(20).forEach(System.out::println);
-		 * 
-		 * System.out.println("------------------------"); Stream<String>
-		 * strm=Stream.<String>builder().add("vishal").add("karam").build();
-		 * strm.forEach(System.out::println);
-		 * 
-		 * System.out.println("------------------------");
-		 * Stream.generate(()->"hello").limit(2).forEach(s->System.out.println(s));
-		 * System.out.println("------------------------");
-		 * 
-		 * IntStream is = IntStream.range(0, 5); IntStream isc =
-		 * IntStream.rangeClosed(0, 5); is.forEach(System.out::println);
+		Supplier<UUID> uuid = UUID::randomUUID;
+		Stream<UUID> generate = Stream.generate(uuid);
+		List<UUID> list = generate.limit(10).skip(15).collect(Collectors.toList());
+		list.forEach(l -> System.out.println(l));
 
+		System.out.println("------------------------");
+
+		Stream<Integer> iterate = Stream.iterate(0, i -> i + 1);
+		iterate.skip(30).limit(20).forEach(System.out::println);
+
+		System.out.println("------------------------");
+		Stream<String> strm = Stream.<String>builder().add("vishal").add("karam").build();
+		strm.forEach(System.out::println);
+
+		System.out.println("------------------------");
+		Stream.generate(() -> "hello").limit(2).forEach(s -> System.out.println(s));
+		System.out.println("------------------------");
+
+		IntStream is = IntStream.range(0, 5);
+		IntStream isc = IntStream.rangeClosed(0, 5);
+		is.forEach(System.out::println);
 		System.out.println("------------------------");
 
 		LongStream ls = LongStream.range(0, 5);
 		LongStream lsc = LongStream.rangeClosed(0, 5);
 		lsc.forEach(System.out::println);
-		 */
-		
+
 		System.out.println("------------------------");
 
-		Random rad=new Random();
-		DoubleStream ds=rad.doubles(2);
+		Random rad = new Random();
+		DoubleStream ds = rad.doubles(2);
 		ds.forEach(System.out::println);
-
 
 	}
 
